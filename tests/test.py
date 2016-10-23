6dc1f67878dbb32591f07b0e8c8ccaa15613b0d7
phapli <phapli1991@gmail.com>
2016-10-24 01:38:20 +0700

    change test script to input full cmd

--- tests/test.py
+++ tests/test.py
@@ -17,11 +17,10 @@ class RF_Controller:
 
 	def __init__(self, port, baudrate, timeout):
 		""" Constructor for RF_Controller
-
-		Args:
-	 	port (int): rf port
-			baudrate (int): rf baudrate
-			timeout (int): rf timeout
+	Args:
+	port (int): rf port
+	baudrate (int): rf baudrate
+	timeout (int): rf timeout
 		Returns:
 		"""
 		self.ser.port = port
@@ -29,15 +28,12 @@ class RF_Controller:
 		self.ser.timeout = timeout
 		self.ser.open()
 		print self.ser.is_open
-
-
 	def read(self):
 		""" Read RF data
-
-		Args:
-	 	port (int): rf port
-			baudrate (int): rf baudrate
-			timeout (int): rf timeout
+	Args:
+	port (int): rf port
+	baudrate (int): rf baudrate
+	timeout (int): rf timeout
 		Returns:
 		"""
 		temp_buff_read = self.ser.read(5)
@@ -51,7 +47,6 @@ class RF_Controller:
 
 	def write(self, data):
 		""" Write RF data
-
 		Args:
 			data (bytes): data
 		Returns:
@@ -61,7 +56,6 @@ class RF_Controller:
 
 	def write_process(self, room):
 		""" Write CMD_PROCESS to 1 special room.
-
 		Args:
 			room (byte): room target
 		Returns:
@@ -71,10 +65,9 @@ class RF_Controller:
 
 	def write_ack(self, cmd, room):
 		""" Write CMD_ACK.
-
-		Args:
-			cmd (byte): cmd received
-			room (byte): room target
+	Args:
+	cmd (byte): cmd received
+	room (byte): room target
 		Returns:
 		"""	
 		data = bytearray([self.CMD_ACK, cmd, room, 0x00, 0x00])
@@ -105,9 +98,12 @@ class RF_Controller:
 rf_controller = RF_Controller(RF_PORT, RF_BAUDRATE, 0.2)
 
 for line in fileinput.input():
-    print "input: " + line
-    if line == "1\n":
-		print "here"
-		data = bytearray([0x01, 0x01, 0x15, 0x3d, 0x2f])
-		rf_controller.write(data)
-    pass
+	print "input: " + line
+	rf_controller.write(binascii.unhexlify(line.strip()))
+	 # if line == "1\n":
+	# 	data = bytearray([0x01, 0x01, 0x15, 0x3d, 0x2f])
+	# 	rf_controller.write(data)
+	# elif line == "2\n"
+	# 	data = bytearray([0x01, 0x01, 0x15, 0x3d, 0x2f])
+	# 	rf_controller.write(data)
+	pass
